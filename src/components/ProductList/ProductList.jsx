3c31904be75a5fdc1492872a4e7eb1d633239404
halbuflasa <albuflasahessa@gmail.com>
2024-11-24 15:27:36 +0300

    Product list  component

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,15 +1,22 @@
-import Stack from 'react-bootstrap/Stack';
+// src/components/ProductList/ProductList.jsx
+import { Link } from 'react-router-dom';
 
-function ProductList() {
-    return (
-        <>
-            <Stack direction="horizontal" gap={3}>
-                <div className="p-2">First item</div>
-                <div className="p-2">Second item</div>
-                <div className="p-2">Third item</div>
-            </Stack>
-        </>
-    )
-}
+const ProductList = (props) => {
+  return (
+    <div>
+      <h1>Product List</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+        {props.products.map((product) => (
+          <div key={product._id} style={{ border: '1px solid #ccc', padding: '20px', width: '200px' }}>
+            <h2><Link to={`/products/${product._id}`}>{product.product_name}</Link></h2>
+            <p>Category: {product.category}</p>
+            <p>Sub-category: {product.sub_category}</p>
+            <p>Cost: ${product.product_cost_to_consumer}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
